8e23f59356569114fec6d5057fd65d1ab2198a61
Nikita <dolganov.alfa@gmail.com>
2023-12-10 13:27:57 +0300

    dz_1

--- dz_1_14_12_23/Program.cs
+++ dz_1_14_12_23/Program.cs
@@ -2,8 +2,20 @@
 
 
 int[] array = new int[10];
-int number = Convert.ToInt32(Console.ReadLine());
+int count = 0;
 
+for(int i = 0; i<array.Length;i++)
+{
+    array[i]= new Random().Next(1,101);
+    Console.WriteLine(array[i]+ " ");
+
+    if (array[i]>=20 && array[i]<=90)
+    {
+        count ++;
+    }
+}
+
+Console.WriteLine($"{count} лежит в промежутке");
 
 
 
