45d111a07e7f61922e18224f5a969566222e059e
Nikita <dolganov.alfa@gmail.com>
2023-12-20 13:09:02 +0300

    dz61

--- dz_6/Program.cs
+++ dz_6/Program.cs
@@ -1,43 +1,6 @@
 ﻿// //Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
-// int[,] array = new int[3,3];
 
-// for (int i = 0; i < array.GetLength(0); i++)
-// {
-//     for (int j = 0; j < array.GetLength(1); j++)
-//     {
-//         array[i,j] = new Random().Next(1,10);
-//         System.Console.Write("{0}", array[i,j] + " ");
-//     }
-//     System.Console.WriteLine();
-// }
-
-// int sum = 0;
-// for (int j = 0; j < array.GetLength; ++j)
-// {
-//     for (int i = 0; i < array.GetLength; ++i)
-//     {
-//         sum = sum + array[i][j];
-//     }
-//     Console.WriteLine(String.Format("Summation of {0}th column is {1}", j,sum)); 
-//     sum=0;       
-// }
-
-//int minSum = int.MaxValue;
-    // int minSumLine = 0;
-    // for (int i = 0; i < array.GetLength(0); i++)
-    // {
-    //     int sum1 = array[0,0];
-    //     for (int j = 0; j < array.GetLength(1); j++)
-    //     {
-    //         sum1+=array[i,j];
-    //     }
-    //     if (sum1 < minSum)
-    //     {
-    //         minSum = sum1;
-    //         minSumLine = i;
-    //     }
-    // }
 
 int [,] array = new int[3,3];
 
