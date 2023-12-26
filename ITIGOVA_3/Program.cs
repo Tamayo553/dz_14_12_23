aa35b370b64907b8d3bd22a97f64b2e7146aa59c
Nikita <dolganov.alfa@gmail.com>
2023-12-26 20:05:33 +0300

    dad

---
+++ ITIGOVA_3/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+class Program 
+{
+    static void Main() {
+        
+        int[] array = {34 , 15 , 74, 85, 0,7};
+
+        
+        PrintArray(array, array.Length - 1);
+    }
+    static void PrintArray(int[] array, int index) 
+    {
+        if (index < 0) 
+        {
+            return;
+        }
+
+        Console.Write(" ");
+        Console.Write(array [ index ]);
+
+        
+        PrintArray(array, index - 1);
+    }
+}
+
